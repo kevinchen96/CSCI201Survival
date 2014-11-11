d8bda230de9d662b8747a6a664f6f0f221208d3e
System Administrator <root@Daniels-MacBook-Pro.local>
2014-11-11 02:15:46 -0800

    Game Panel added

---
+++ src/game/GamePanel.java
@@ -0,0 +1,88 @@
+package game;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel  implements Runnable, KeyListener {
+	private static String TITLE = new String("Survival");
+	private static JFrame FRAME = new JFrame(TITLE);
+	public static int WIDTH = 800;
+	public static int HEIGHT = 600;
+	private Graphics2D g;
+	private BufferedImage image;
+	
+	public GamePanel(){
+		g = (Graphics2D) getGraphics();
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		addKeyListener(this);
+		
+		FRAME.setSize(WIDTH, HEIGHT);
+		FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FRAME.setFocusable(true);
+		FRAME.requestFocus();
+		
+		FRAME.add(this);
+		
+		FRAME.setVisible(true);
+	
+		run();
+	}
+
+
+	@Override
+	public void run() {
+		long start = System.nanoTime(); 
+		double elapsed;
+		
+		while(true){
+			tick();
+			
+			elapsed = (double)(System.nanoTime() - start) / 1000000.0;
+			if(elapsed >= 60){
+				FRAME.setTitle(TITLE + "       ||       FPS: " + elapsed);
+				render();
+				drawToScreen();
+			}
+			
+		}
+	}
+	
+	//handles logic and server communication
+	public void tick(){
+		
+	}
+	
+	//handles graphics 
+	public void render(){
+		
+	}
+	
+	//refreshes screen
+	public void drawToScreen(){
+		Graphics2D g2 = (Graphics2D) getGraphics();
+		g2.drawImage(image, 0, 0, null);
+		g2.dispose();
+		
+	}
+	
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
+		
+	}
+	
+	public static void main(String[] args){
+		new GamePanel();
+	}
+}
