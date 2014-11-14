39e13190430e4edf3777eb2332d9a308e23db0f2
Kevin Chen <kevinchen96@gmail.com>
2014-11-14 02:04:48 -0800

    play state new

---
+++ src/GameStates/PlayState.java
@@ -0,0 +1,42 @@
+package GameStates;
+
+import java.awt.Graphics2D;
+
+public class PlayState extends States {
+
+	public PlayState(GameStates gameStates) {
+		// TODO Auto-generated constructor stub
+		manager = gameStates;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(int k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(int k) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
