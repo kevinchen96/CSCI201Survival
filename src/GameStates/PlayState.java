6e6bbd38e5e0e7821a1517f982081465c07ff8eb
Kevin Chen <kevinchen96@gmail.com>
2014-11-14 03:25:30 -0800

    Reclone

--- src/GameStates/PlayState.java
+++ src/GameStates/PlayState.java
@@ -3,34 +3,21 @@ package GameStates;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 
-<<<<<<< HEAD
-public class PlayState extends States {
-
-	public PlayState(GameStates gameStates) {
-		// TODO Auto-generated constructor stub
-		manager = gameStates;
-=======
 import Map.TileMap;
 
 public class PlayState extends States{
 	
 	TileMap map;
 	
-	public PlayState(){
-		
->>>>>>> 48b927f906fec7368a0e3fcdcea992321e5bfa88
+	public PlayState(GameStates gameStates){
+		manager = gameStates;
 	}
 
 	@Override
 	public void init() {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-
-=======
 		map = new TileMap(30);
 		map.loadTiles("src/resources/tilesets/grasstileset.gif");
 		map.loadMap("src/resources/maps/level1-1.map");
->>>>>>> 48b927f906fec7368a0e3fcdcea992321e5bfa88
 	}
 
 	@Override
@@ -43,14 +30,12 @@ public class PlayState extends States{
 	public void draw(Graphics2D g) {
 		// TODO Auto-generated method stub
 		g.drawString("In PlayState", 277, 180);
+		map.render(g);
 	}
 
 	@Override
 	public void keyPressed(int k) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
 
-=======
 		//movement 
 		if(k == KeyEvent.VK_UP){
 			
@@ -61,7 +46,6 @@ public class PlayState extends States{
 		}else if(k == KeyEvent.VK_LEFT){
 			
 		}
->>>>>>> 48b927f906fec7368a0e3fcdcea992321e5bfa88
 	}
 
 	@Override
